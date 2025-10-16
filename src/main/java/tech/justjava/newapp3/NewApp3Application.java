181d5c1116d52e9c8837625a88fc08008558165d
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2025-10-16 13:12:53 +0100

    Initial commit: Spring Boot starter auto-generated by Agentic AI

---
+++ src/main/java/tech/justjava/newapp3/NewApp3Application.java
@@ -0,0 +1,13 @@
+package tech.justjava.newapp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewApp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/justjava/newapp3/NewApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.justjava.newapp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
