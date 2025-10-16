75684bcb1b3d403edd991e86d0c3a674b103a03d
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2025-10-16 13:18:34 +0100

    Automated update by SpringAI ProjectAutomationTools

---
+++ src/main/java/tech/justjava/newapp3/controller/HomeController.java
@@ -0,0 +1,14 @@
+    package tech.justjava.newapp3.controller;
+
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
+
+    @Controller
+    public class HomeController {
+        @GetMapping("/")
+        public String index(Model model) {
+            model.addAttribute("appName", "NewApp3");
+            return "home";
+        }
+    }
